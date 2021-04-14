68a7b9b7000ebf5359944c8563648cec87dd25f7
Dimitri Nakos <diminako@Dimitris-MacBook-Air.local>
2021-04-13 23:20:43 -0400

    just tinkering

---
+++ day-01/day-01.py
@@ -0,0 +1,21 @@
+# Day 01 Variables
+
+print('Dimitri')
+print(2+2)
+print("Hello " + 'World!')
+print("Hello World!")
+
+print(len("Dimitri"))
+
+name = 'Dimitri'
+print(len(name))
+print(len(name)+5)
+
+#1. Create a greeting for your program.
+print('Hello World this is the Band Name Generator!')
+#2. Ask the user for the city that they grew up in.
+location = input('Where did you grow up at?\n')
+#3. Ask the user for the name of a pet.
+pet = input('What is your pets name?\n')
+#4. Combine the name of their city and pet and show them their band name.
+print('Your band name is ' + pet + ' ' + location + '.')
